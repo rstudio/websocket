00aba35a70a128cd586d82d2c815ec0ae3fefad8
Alan Dipert <alan@dipert.org>
2018-02-21 13:36:07 -0800

    Add temporary R websocket server for testing

---
+++ tmp/websocketServer.R
@@ -0,0 +1,37 @@
+# A little example server that Winston made for testing.
+
+library(httpuv)
+startServer("0.0.0.0", 8080,
+  list(
+    onWSOpen = function(ws) {
+      cat("onWSOpen\n")
+
+      ws$onMessage(function(binary, message) {
+        cat("server received message:", message, "\n")
+      })
+
+      is_open <- TRUE
+      count <- 0
+      send_func <- function() {
+        if (is_open) {
+          count <<- count + 1
+          ws$send(count)
+
+          if (count == 5) {
+            cat("Closing connection in 1 second\n")
+            later::later(ws$close, 1)
+          } else {
+            later::later(send_func, 1)
+          }
+        }
+      }
+      send_func()
+
+      ws$onClose(function() {
+        is_open <<- FALSE
+      })
+
+    }
+  )
+)
+#stopAllServers()
