cb334d47bcaabd03909c53d2133e4de76600d8f6
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2018-02-22 18:50:42 +0000

    updated scratch.R example

--- tmp/scratch.R
+++ tmp/scratch.R
@@ -1,5 +1,9 @@
 
-ws <- WebsocketClient$new("ws://localhost:8080/")
+ws <- WebsocketClient$new("ws://localhost:8080/",
+  onMessage = function(msg) cat(msg),
+  onDisconnected = function() "you're disconnected"
+)
+
 ws$connect()
 ws$send("whoa")
 ws$close()
