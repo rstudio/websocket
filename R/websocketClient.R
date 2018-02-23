e9ab63748ea0d60671771f2ba3ac7fc96d6963e0
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2018-02-23 22:57:36 +0000

    added documentation

--- R/websocketClient.R
+++ R/websocketClient.R
@@ -3,6 +3,39 @@
 #' @include RcppExports.R
 NULL
 
+#' Create a websocket client
+#'
+#' @section Usage:
+#' \preformatted{WebsocketClient$new(url, onMessage = identity, onDisconnected = identity)}
+#'
+#' @param url The websocket URL.
+#' @param onMessage A function called for each message received from the server.
+#'   Must take a single argument, the message string.
+#' @param onDisconnected A function called with no arguments when either the client
+#'   or the server disconnect.
+#'
+#' @return a WebsocketClient instance
+#'
+#' @name WebsocketClient
+#'
+#' @examples
+#' # Create a websocket using the websocket.org test server
+#' ws <- WebsocketClient$new("ws://echo.websocket.org/",
+#'   onMessage = function(msg) {
+#'     cat("Client got msg: ", msg, "\n")
+#'   },
+#'   onDisconnected = function() {
+#'     cat("Client disconnected\n")
+#'   }
+#' )
+#'
+#' # Send a message
+#' ws$send("hello")
+#'
+#' # Close the websocket after we're done with it
+#' ws$close()
+NULL
+
 #' @export
 WebsocketClient <- R6::R6Class("WebsocketClient",
   public = list(
