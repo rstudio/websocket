3c9e5612a48f7cb172ed79256d065c8231013b7b
Winston Chang <winston@stdout.org>
2018-04-11 10:55:22 -0500

    First step to using websocketpp for both ws and wss

---
+++ src/client.hpp
@@ -0,0 +1,114 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/config/asio_client.hpp>
+#include <websocketpp/client.hpp>
+
+
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+typedef websocketpp::lib::function<void(websocketpp::connection_hdl, message_ptr)> message_handler;
+typedef websocketpp::lib::function<void(websocketpp::connection_hdl)> close_handler;
+typedef websocketpp::client<websocketpp::config::asio_tls_client> wss_client;
+typedef websocketpp::client<websocketpp::config::asio_client>     ws_client;
+
+class Client {
+public:
+  virtual void set_access_channels(websocketpp::log::level channels) = 0;
+  virtual void clear_access_channels(websocketpp::log::level channels) = 0;
+  virtual void init_asio() = 0;
+  virtual void set_tls_init_handler(websocketpp::transport::asio::tls_socket::tls_init_handler h) = 0;
+  virtual void set_open_handler(websocketpp::open_handler h) = 0;
+  virtual void set_message_handler(message_handler h) = 0;
+  virtual void set_close_handler(close_handler h) = 0;
+  virtual void set_fail_handler(websocketpp::fail_handler h) = 0;
+
+  virtual void setup_connection(std::string location, websocketpp::lib::error_code &ec) = 0;
+  virtual void connect() = 0;
+  virtual std::size_t run_one() = 0;
+  virtual websocketpp::lib::asio::io_service& get_io_service() = 0;
+  virtual std::size_t poll() = 0;
+  virtual void send(std::string const& payload, websocketpp::frame::opcode::value op) = 0;
+  virtual void reset() = 0;
+  virtual void close(websocketpp::close::status::value const code, std::string const & reason) = 0;
+  virtual bool stopped() = 0;
+};
+
+
+template <class T>
+class ClientImpl : public Client {
+  // struct Trait { typedef
+
+public:
+  ClientImpl<T> () {};
+
+  void set_access_channels(websocketpp::log::level channels) {
+    client.set_access_channels(channels);
+  };
+  void clear_access_channels(websocketpp::log::level channels) {
+    client.clear_access_channels(channels);
+  };
+  void init_asio() {
+    client.init_asio();
+  };
+  void set_tls_init_handler(websocketpp::transport::asio::tls_socket::tls_init_handler h);
+  void set_open_handler(websocketpp::open_handler h) {
+    client.set_open_handler(h);
+  };
+  void set_message_handler(message_handler h) {
+    client.set_message_handler(h);
+  };
+  void set_close_handler(close_handler h) {
+    client.set_close_handler(h);
+  };
+  void set_fail_handler(websocketpp::fail_handler h) {
+    client.set_fail_handler(h);
+  };
+
+  void setup_connection(std::string location, websocketpp::lib::error_code &ec) {
+    this->con = client.get_connection(location, ec);
+  };
+  void connect() {
+    client.connect(this->con);
+  };
+  std::size_t run_one() {
+    return client.run_one();
+  };
+  websocketpp::lib::asio::io_service& get_io_service() {
+    return client.get_io_service();
+  };
+  std::size_t poll() {
+    return client.poll();
+  };
+  void send(std::string const& payload, websocketpp::frame::opcode::value op) {
+    client.send(this->con, payload, op);
+  };
+  void reset() {
+    client.reset();
+  };
+  void close(websocketpp::close::status::value const code, std::string const& reason) {
+    client.close(this->con, code, reason);
+  };
+  bool stopped() {
+    return client.stopped();
+  };
+
+private:
+  T client;
+  typename T::connection_ptr con;
+};
+
+
+
+// Specializations for set_tls_init_handler()
+template <>
+void ClientImpl<wss_client>::set_tls_init_handler(websocketpp::transport::asio::tls_socket::tls_init_handler h) {
+  client.set_tls_init_handler(h);
+};
+
+template <>
+void ClientImpl<ws_client>::set_tls_init_handler(websocketpp::transport::asio::tls_socket::tls_init_handler h) {
+  throw std::runtime_error("Can't set TLS init handler for ws:// connection.");
+};
+
+#endif
--- src/wssClient.cpp
+++ src/wssClient.cpp
@@ -1,16 +1,10 @@
 #include <Rcpp.h>
 #define ASIO_STANDALONE
-#include <websocketpp/config/asio_client.hpp>
-#include <websocketpp/client.hpp>
 #include <iostream>
-#include <boost/make_shared.hpp>
-#include <boost/shared_ptr.hpp>
+#include <websocketpp/common/functional.hpp>
+#include "client.hpp"
 
-typedef std::shared_ptr<asio::ssl::context> context_ptr;
-typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
-typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
-typedef websocketpp::lib::function<void(websocketpp::connection_hdl, message_ptr)> message_handler;
-typedef websocketpp::lib::function<void(websocketpp::connection_hdl)> close_handler;
+typedef websocketpp::lib::shared_ptr<asio::ssl::context> context_ptr;
 
 using namespace Rcpp;
 using websocketpp::lib::placeholders::_1;
@@ -18,7 +12,7 @@ using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
 static context_ptr on_tls_init() {
-  context_ptr ctx = std::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
+  context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
   try {
     ctx->set_options(asio::ssl::context::default_workarounds |
       asio::ssl::context::no_sslv2 |
@@ -30,22 +24,24 @@ static context_ptr on_tls_init() {
   return ctx;
 }
 
-struct WSSConnection {
+class WSConnection {
+public:
+  WSConnection(websocketpp::lib::shared_ptr<Client> client) : client(client) {};
+
   enum STATE { INIT, OPEN, CLOSED, FAILED };
   STATE state = INIT;
-  client client;
-  client::connection_ptr con;
+  websocketpp::lib::shared_ptr<Client> client;
 };
 
-boost::shared_ptr<WSSConnection> xptrGetClient(SEXP client_xptr) {
+websocketpp::lib::shared_ptr<WSConnection> xptrGetClient(SEXP client_xptr) {
   if (TYPEOF(client_xptr) != EXTPTRSXP) {
     throw Rcpp::exception("Expected external pointer.");
   }
-  return *reinterpret_cast<boost::shared_ptr<WSSConnection>*>(R_ExternalPtrAddr(client_xptr));
+  return *reinterpret_cast<websocketpp::lib::shared_ptr<WSConnection>*>(R_ExternalPtrAddr(client_xptr));
 }
 
 void client_deleter(SEXP client_xptr) {
-  delete reinterpret_cast<boost::shared_ptr<WSSConnection>*>(R_ExternalPtrAddr(client_xptr));
+  delete reinterpret_cast<websocketpp::lib::shared_ptr<WSConnection>*>(R_ExternalPtrAddr(client_xptr));
   R_ClearExternalPtr(client_xptr);
 }
 
@@ -54,42 +50,60 @@ void handleMessage(Rcpp::Function onMessage, websocketpp::connection_hdl, messag
   onMessage(payload);
 }
 
-void handleClose(boost::shared_ptr<WSSConnection> wssPtr, Rcpp::Function onClose, websocketpp::connection_hdl) {
-  wssPtr->state = WSSConnection::STATE::CLOSED;
+void handleClose(websocketpp::lib::shared_ptr<WSConnection> wssPtr, Rcpp::Function onClose, websocketpp::connection_hdl) {
+  wssPtr->state = WSConnection::STATE::CLOSED;
   onClose();
 }
 
-void handleOpen(boost::shared_ptr<WSSConnection> wssPtr, Rcpp::Function onOpen, websocketpp::connection_hdl) {
-  wssPtr->state = WSSConnection::STATE::OPEN;
+void handleOpen(websocketpp::lib::shared_ptr<WSConnection> wssPtr, Rcpp::Function onOpen, websocketpp::connection_hdl) {
+  wssPtr->state = WSConnection::STATE::OPEN;
   onOpen();
 }
 
-void handleFail(boost::shared_ptr<WSSConnection> wssPtr, Rcpp::Function onFail, websocketpp::connection_hdl) {
-  wssPtr->state = WSSConnection::STATE::FAILED;
+void handleFail(websocketpp::lib::shared_ptr<WSConnection> wssPtr, Rcpp::Function onFail, websocketpp::connection_hdl) {
+  wssPtr->state = WSConnection::STATE::FAILED;
   onFail();
 }
 
 // [[Rcpp::export]]
 SEXP wssCreate(std::string uri, Rcpp::Function onMessage, Rcpp::Function onOpen, Rcpp::Function onClose, Rcpp::Function onFail) {
-  boost::shared_ptr<WSSConnection> wssPtr = boost::make_shared<WSSConnection>();
+  if (uri.size() < 6) {
+    throw Rcpp::exception("Invalid websocket URI: too short");
+  }
 
-  wssPtr->client.set_access_channels(websocketpp::log::alevel::all);
-  wssPtr->client.clear_access_channels(websocketpp::log::alevel::frame_payload);
-  wssPtr->client.init_asio();
-  wssPtr->client.set_tls_init_handler(bind(&on_tls_init));
-  wssPtr->client.set_open_handler(bind(handleOpen, wssPtr, onOpen, ::_1));
-  wssPtr->client.set_message_handler(bind(handleMessage, onMessage, ::_1, ::_2));
-  wssPtr->client.set_close_handler(bind(handleClose, wssPtr, onClose, ::_1));
-  wssPtr->client.set_fail_handler(bind(handleFail, wssPtr, onFail, ::_1));
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr;
+
+  bool tls = false;
+  if (uri.substr(0, 5) == "ws://") {
+    websocketpp::lib::shared_ptr<ClientImpl<ws_client>> client = websocketpp::lib::make_shared<ClientImpl<ws_client>>();
+    wssPtr = websocketpp::lib::make_shared<WSConnection>(client);
+  } else if (uri.substr(0, 6) == "wss://") {
+    tls = true;
+    websocketpp::lib::shared_ptr<ClientImpl<wss_client>> client = websocketpp::lib::make_shared<ClientImpl<wss_client>>();
+    wssPtr = websocketpp::lib::make_shared<WSConnection>(client);
+  } else {
+    throw Rcpp::exception("Invalid websocket URI: must begin with ws:// or wss://");
+  }
+
+  wssPtr->client->set_access_channels(websocketpp::log::alevel::all);
+  wssPtr->client->clear_access_channels(websocketpp::log::alevel::frame_payload);
+  wssPtr->client->init_asio();
+  if (tls) {
+    wssPtr->client->set_tls_init_handler(bind(&on_tls_init));
+  }
+  wssPtr->client->set_open_handler(bind(handleOpen, wssPtr, onOpen, ::_1));
+  wssPtr->client->set_message_handler(bind(handleMessage, onMessage, ::_1, ::_2));
+  wssPtr->client->set_close_handler(bind(handleClose, wssPtr, onClose, ::_1));
+  wssPtr->client->set_fail_handler(bind(handleFail, wssPtr, onFail, ::_1));
 
   websocketpp::lib::error_code ec;
-  wssPtr->con = wssPtr->client.get_connection(uri, ec);
+  wssPtr->client->setup_connection(uri, ec);
   if (ec) {
     // TODO Should we call onFail here?
     stop("Could not create connection because: " + ec.message());
   }
 
-  boost::shared_ptr<WSSConnection> *extWssPtr = new boost::shared_ptr<WSSConnection>(wssPtr);
+  websocketpp::lib::shared_ptr<WSConnection> *extWssPtr = new websocketpp::lib::shared_ptr<WSConnection>(wssPtr);
   SEXP client_xptr = PROTECT(R_MakeExternalPtr(extWssPtr, R_NilValue, R_NilValue));
   R_RegisterCFinalizerEx(client_xptr, client_deleter, TRUE);
   UNPROTECT(1);
@@ -98,59 +112,59 @@ SEXP wssCreate(std::string uri, Rcpp::Function onMessage, Rcpp::Function onOpen,
 
 // [[Rcpp::export]]
 void wssConnect(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.connect(wssPtr->con);
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->connect();
   // Block until the connection is either open, closed, or the attempt to connect has failed.
-  // wssPtr->client.run() would block indefinitely, so we use run_one() instead.
+  // wssPtr->client->run() would block indefinitely, so we use run_one() instead.
   // We don't need to call restart() here because the underlying io_context is never out of work between invocations.
-  while (wssPtr->state == WSSConnection::STATE::INIT) {
-    wssPtr->client.run_one();
+  while (wssPtr->state == WSConnection::STATE::INIT) {
+    wssPtr->client->run_one();
   }
 }
 
 // [[Rcpp::export]]
 void wssRestart(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.get_io_service().restart();
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->get_io_service().restart();
 }
 
 // [[Rcpp::export]]
 void wssPoll(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.poll();
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->poll();
 }
 
 // [[Rcpp::export]]
 void wssSend(SEXP client_xptr, std::string msg) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.send(wssPtr->con, msg, websocketpp::frame::opcode::text);
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->send(msg, websocketpp::frame::opcode::text);
 }
 
 // [[Rcpp::export]]
 void wssReset(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.reset();
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->reset();
 }
 
 // [[Rcpp::export]]
 void wssClose(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  wssPtr->client.close(wssPtr->con, websocketpp::close::status::normal, "closing normally");
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  wssPtr->client->close(websocketpp::close::status::normal, "closing normally");
 }
 
 // [[Rcpp::export]]
 bool wssStopped(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
-  return wssPtr->client.stopped();
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
+  return wssPtr->client->stopped();
 }
 
 // [[Rcpp::export]]
 std::string wssState(SEXP client_xptr) {
-  boost::shared_ptr<WSSConnection> wssPtr = xptrGetClient(client_xptr);
+  websocketpp::lib::shared_ptr<WSConnection> wssPtr = xptrGetClient(client_xptr);
   switch(wssPtr->state) {
-    case WSSConnection::STATE::INIT: return "INIT";
-    case WSSConnection::STATE::OPEN: return "OPEN";
-    case WSSConnection::STATE::CLOSED: return "CLOSED";
-    case WSSConnection::STATE::FAILED: return "FAILED";
+    case WSConnection::STATE::INIT: return "INIT";
+    case WSConnection::STATE::OPEN: return "OPEN";
+    case WSConnection::STATE::CLOSED: return "CLOSED";
+    case WSConnection::STATE::FAILED: return "FAILED";
   }
 }
