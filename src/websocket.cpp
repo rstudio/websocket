29cc39f77758c18ff5a24eba4ac8f3f483d1f31b
Winston Chang <winston@stdout.org>
2018-05-29 16:22:53 -0500

    Undefine Realloc and Free

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -1,4 +1,12 @@
 #include <Rcpp.h>
+
+// https://stackoverflow.com/a/11593943/412655
+// Taken from http://tolstoy.newcastle.edu.au/R/e2/devel/06/11/1242.html
+// Undefine the Realloc macro, which is defined by both R and by Windows stuff
+#undef Realloc
+// Also need to undefine the Free macro
+#undef Free
+
 #define ASIO_STANDALONE
 #include <iostream>
 #include <websocketpp/common/functional.hpp>
